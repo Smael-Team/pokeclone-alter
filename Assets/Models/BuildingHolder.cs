6e002d601458324e28efe833b587912dcb67b9c3
dcheong <douglas.cheong1000@gmail.com>
2016-10-01 23:05:52 -0400

    added trees

--- Assets/Models/BuildingHolder.cs
+++ Assets/Models/BuildingHolder.cs
@@ -26,12 +26,46 @@ namespace Assets.Models
             if (IsModelCreated)
                 return null;
 
-            var m = new GameObject().AddComponent<BuildingPolygon>();
-            m.gameObject.transform.position = Center;
-            m.gameObject.GetComponent<Renderer>().material = Resources.Load("buildingMaterial") as Material;
-            m.Initialize(_verts);
+            GameObject m;
+            float sArea = SurfaceArea(_verts);
+            Debug.Log(sArea);
+            if (sArea > 300)
+            {
+                m = (GameObject)GameObject.Instantiate(Resources.Load("tree"));
+            } else
+            {
+                m = (GameObject)GameObject.Instantiate(Resources.Load("house1"));
+            }
+            //var m = new GameObject().AddComponent<BuildingPolygon>();
+            m.transform.position = Center;
+            //m.gameObject.GetComponent<Renderer>().material = Resources.Load("buildingMaterial") as Material;
+            //m.Initialize(_verts);
             IsModelCreated = true;
             return m.gameObject;
+        }
+
+        float SurfaceArea(List<Vector3> verts)
+        {
+            var list = verts.Select(x => new Vector3(x.x, 0, x.z)).ToList();
+            float temp = 0;
+            int i = 0;
+            for (; i < list.Count; i++)
+            {
+                if (i != list.Count - 1)
+                {
+                    float mulA = list[i].x * list[i + 1].z;
+                    float mulB = list[i + 1].x * list[i].z;
+                    temp = temp + (mulA - mulB);
+                }
+                else
+                {
+                    float mulA = list[i].x * list[0].z;
+                    float mulB = list[0].x * list[i].z;
+                    temp = temp + (mulA - mulB);
+                }
+            }
+            temp *= 0.5f;
+            return Mathf.Abs(temp);
         }
 
     }
